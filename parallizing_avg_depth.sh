862b3c76a62a7c1b304fd23497728a0248d202ec
Candice Johnson <u6049732@lp101.lonepeak.peaks>
2023-04-20 14:55:26 -0600

    read depth

---
+++ parallizing_avg_depth.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#SBATCH --account=utu-biol4310
+#SBATCH --partition=lonepeak
+#SBATCH --nodes=1
+#SBATCH --ntasks=6
+#SBATCH -o slurm-%j.out-%N
+#SBATCH -e slurm-%j.err-%N
+
+
+# create working environment
+wd=/scratch/general/nfs1/utu_4310/whiptail_shared_data/mapped_reads/su
+mkdir $wd/depth
+
+bash parallel_bam.sh -i $wd -o $wd/depth
+
