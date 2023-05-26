7930a2d3c10f3cedea12b9977baa3843b5d2113b
Candice Johnson <u6049732@lonepeak1.wasatch.peaks>
2023-05-25 21:21:23 -0600

    count reads for each species as well as read1 and read2

---
+++ unmerged_read_count.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+{
+usage="$(basename "$0") [-h] [-i <working_directory] [-o <output_directory>]This program will trim raw reads.
+    -h  show this help text
+    -i  Path to the working directory (the main directory for the raw reads)    -o  Path to the output directory (the main directory for the clean reads)"
+options=':h:i:o:'
+while getopts $options option; do
+  case "$option" in
+    h) echo "$usage"; exit;;
+    i) i=$OPTARG;;
+    o) o=$OPTARG;;
+    :) printf "missing argument for -%s\n" "$OPTARG" >&2; echo "$usage" >&2; exit 1;;
+   \?) printf "illegal option: -%s\n" "$OPTARG" >&2; echo "$usage" >&2; exit 1;;
+  esac
+done
+
+
+echo ""
+echo "Working directory: " $i
+echo "Output directory: " $o
+echo ""
+
+
+# mandatory arguments
+if [ ! "$i" ] || [ ! "$o" ]; then
+  echo "arguments -o, and -i  must be provided"
+  echo "$usage" >&2; exit 1
+fi
+
+
+#Get number of unmerged reads from fastp for each species
+#a. Count number of unmerged for read1, then count number of unmerged for read2
+
+#A. septemvittatus 
+    #KLC098_USD16091388L_HKFJFDSXX_L4_1.fq.gz
+    #KLC098_USD16091388L_HKFJFDSXX_L4_2.fq.gz
+    #KLC098_USD16091388L_HKG5MDSXX_L1_1.fq.gz
+    #KLC098_USD16091388L_HKG5MDSXX_L1_2.fq.gz
+
+#whole species read count
+zcat KLC098*fq.gz | wc -l
+
+#read1 count 
+ls KLC098*fq.gz | cut -d "." -f "1,2,3,4" | 
+
+
+# A. gularis
+    #RLK004_USD16091389L_HJNHCDSXX_L3_1.fq.gz
+    #RLK004_USD16091389L_HJNHCDSXX_L3_2.fq.gz
+
+#whole species read count
+zcat RLK004*fq.gz | wc -l
+
+
+
+# A. inornatus
+    #RLK019_USD16091390L_HJMVKDSXX_L3_1.fq.gz
+    #RLK019_USD16091390L_HJMVKDSXX_L3_2.fq.gz
+    #RLK019_USD16091390L_HKG5MDSXX_L1_1.fq.gz
+    #RLK019_USD16091390L_HKG5MDSXX_L1_2.fq.gz
+
+#whole species read count
+zcat RLK019*fq.gz | wc -l
+
+
+
+# A. marmoratus
+    #RLK034_USD16091387L_HJNHCDSXX_L2_1.fq.gz
+    #RLK034_USD16091387L_HJNHCDSXX_L2_2.fq.gz
+
+#whole species read count
+zcat RLK034*fq.gz | wc -l
+
+
+} | tee outfile
