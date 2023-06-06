ad01910288744ba28afffbd4d45753efb6312d63
Dante Celani <u6049734@lonepeak1.wasatch.peaks>
2023-06-06 04:53:59 -0600

    I feel like this should work but it's not working.

--- merge_lanes.sh
+++ merge_lanes.sh
@@ -39,22 +39,22 @@ cd $i
 #merge (merged.bam) files sequenced across multiple lanes
 # KLC098 and RLK019
 # I use "[ -f <file_name> ] && do thing" to check if file exists before doing tasks
-[ -f <KLC098_${g}_merged.bam> ] && samtools merge KLC098_${g}_merged.bam KLC098_*_L4.merged_sorted.bam KLC098_*_L1.merged_sorted.bam
+[ ! -f <KLC098_${g}_merged.bam> ] && samtools merge KLC098_${g}_merged.bam KLC098_*_L4.merged_sorted.bam KLC098_*_L1.merged_sorted.bam
 
-[ -f <RLK019_${g}_merged.bam> ] && samtools merge RLK019_${g}_merged.bam RLK019_*_L3.merged_sorted.bam RLK019_*_L1.merged_sorted.bam
+[ ! -f <RLK019_${g}_merged.bam> ] && samtools merge RLK019_${g}_merged.bam RLK019_*_L3.merged_sorted.bam RLK019_*_L1.merged_sorted.bam
 
 #merge (unmerged.bam) files that were sequenced across multiple lanes
-[ -f <KLC098_${g}_unmerged.bam> ] && samtools merge KLC098_${g}_unmerged.bam KLC098_*_L4.unmerged_sorted.bam KLC098_*_L1.unmerged_sorted.bam
+[ ! -f <KLC098_${g}_unmerged.bam> ] && samtools merge KLC098_${g}_unmerged.bam KLC098_*_L4.unmerged_sorted.bam KLC098_*_L1.unmerged_sorted.bam
 
-[ -f <RLK019_${g}_unmerged.bam> ] && samtools merge RLK019_${g}_unmerged.bam RLK019_*_L3.unmerged_sorted.bam RLK019_*_L1.unmerged_sorted.bam
+[ ! -f <RLK019_${g}_unmerged.bam> ] && samtools merge RLK019_${g}_unmerged.bam RLK019_*_L3.unmerged_sorted.bam RLK019_*_L1.unmerged_sorted.bam
 
 #change names of (merged_sorted.bam) and (unmerged_sorted.bam) files sequenced on a single lane
 # RLK004 and RLK034
-[ -f <RLK004_${g}_merged.bam> ] && cp RLK004*.merged_sorted.bam RLK004_${g}_merged.bam
-[ -f <RLK034_${g}_merged.bam> ] && cp RLK034*.merged_sorted.bam RLK034_${g}_merged.bam
+[ ! -f <RLK004_${g}_merged.bam> ] && cp RLK004*.merged_sorted.bam RLK004_${g}_merged.bam
+[ ! -f <RLK034_${g}_merged.bam> ] && cp RLK034*.merged_sorted.bam RLK034_${g}_merged.bam
 
-[ -f <RLK004_${g}_unmerged.bam> ] && cp RLK004*.unmerged_sorted.bam RLK004_${g}_unmerged.bam
-[ -f <RLK034_${g}_unmerged.bam> ] && cp RLK034*.unmerged_sorted.bam RLK034_${g}_unmerged.bam
+[ ! -f <RLK004_${g}_unmerged.bam> ] && cp RLK004*.unmerged_sorted.bam RLK004_${g}_unmerged.bam
+[ ! -f <RLK034_${g}_unmerged.bam> ] && cp RLK034*.unmerged_sorted.bam RLK034_${g}_unmerged.bam
 
 #files look like this...
 #RLK004_USD16091389L_HJNHCDSXX_L3.unmerged_sorted.bam
