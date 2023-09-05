d5b171e7b87be4011ae6db645bc13ad4a4ca122c
KLab-UT <utt.klab@gmail.com>
2023-09-05 10:45:28 -0600

    modified scripts to plot patristic distance by mapping success

--- make_plots.r
+++ make_plots.r
@@ -4,11 +4,14 @@ library('gridExtra')
 # Set your working directory
 # Use this if not in this directory: setwd(<path-to-dir-with-tre-file>)
 
-# Import data
-dat <- read.csv('MappingData.csv')
-dat$log_dist = log(dat$dist)
+# Import data as dataframe
+dat <- read.csv('depth_and_dist.csv')
+# look at first few rows of data
+head(dat)
+#dat$log_dist = log(dat$dist)
 
 # Phylogenetic distance ~ Mapping success
-mapping_dist <- ggplot(dat, aes(dist, mapping_success)) + geom_point(aes(colour = cat), size = 3) + scale_color_manual(values = c("total_mapping_success" = "#2C7C9F", "unmerged_mapping_success" = "#2C7C9F", "merged_mapping_success" = "#7CBEE4"))
+mapping_dist <- ggplot(dat, aes(patristic_distance, mapping_percentage)) + geom_point(aes(colour = cat), size = 3) + scale_color_manual(values = c("total_mapping_success" = "#2C7C9F", "unmerged_mapping_success" = "#2C7C9F", "merged_mapping_success" = "#7CBEE4"))
+mapping_dist <- ggplot(dat, aes(patristic_distance, map_percentage)) + geom_point(size = 3) 
 
 grid.arrange(merged_mapping_dist, unmerged_mapping_dist, total_mapping_dist, nrow=3)
