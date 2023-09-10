69d2db127016b8216076ead3b80ec75830910bc2
Dante Celani <u6049734@lonepeak2.wasatch.peaks>
2023-09-09 18:50:35 -0600

    added remove_M_line_endings.sh to remove the ^M symbols that appear in vim (they are like newline characters for windows software). tehn i used -j to set thread limit for parallelization in q.map_reads_to_genomes.sh

--- q.temp_map_to_genomes.sh
+++ q.temp_map_to_genomes.sh
@@ -2,8 +2,8 @@
 #SBATCH --account=utu
 #SBATCH --partition=lonepeak
 #SBATCH --time=48:00:00
-#SBATCH --nodes=3
-#SBATCH --ntasks=52
+#SBATCH --nodes=1
+#SBATCH --ntasks=16
 #SBATCH -o slurm-%j.out-%N
 #SBATCH -e slurm-%j.err-%N
 
@@ -60,6 +60,9 @@ export -f MapReads
 # grep -v filter out lines starting with #
 
 #grep -v '^#' ref_genomes.txt | cut -d " " -f 1,2 | parallel MapReads {1} {2}
-grep -v '^#' ref_genomes_2.txt | parallel --colsep ' ' MapReads {1} {2}
+# -j 8 limits the number of threads used to 8
+grep -v '^#' ref_genomes_2.txt | parallel -j 8 --colsep ' ' MapReads {1} {2}
 echo "mapping done"
+
+# Keep in mind that if your MapReads function itself uses multiple threads (e.g., with the -t option set to 4 as in your example), the total number of threads used will be a combination of those used by MapReads and those used by parallel.
 
