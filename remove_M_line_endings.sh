b4985463174c933cdcb24f1cf80848265ec37a27
Dante Celani <u6049734@lonepeak2.wasatch.peaks>
2023-09-23 17:14:16 -0600

    changed intermediate file for remove_M_line_endings.sh

--- remove_M_line_endings.sh
+++ remove_M_line_endings.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-sed 's/\r$//g' depth_and_dist.txt > intermediate.txt
-cp intermediate.txt depth_and_dist.txt
+sed 's/\r$//g' depth_and_dist.txt > .remove_M_line_endings_intermediate.txt 
+cp .remove_M_line_endings_intermediate.txt depth_and_dist.txt
 
 # \r: This represents the carriage return character (commonly used for line endings in Windows text files).
 # $: This matches the end of a line.
