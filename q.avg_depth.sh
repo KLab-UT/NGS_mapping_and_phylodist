2dd5c80083620489208057d642b4901ff5227d37
Dante Celani <u6049734@kingspeak1.wasatch.peaks>
2023-10-26 16:50:06 -0600

    changed name of add_column.sh to add_phylodist_column.sh

--- q.avg_depth.sh
+++ q.avg_depth.sh
@@ -12,7 +12,7 @@ wd=/scratch/general/nfs1/utu_4310/whiptail_shared_data/mapped_reads
 home=~/Biol_4310/NGS_mapping_and_phylodist
 cd $wd
 #mkdir depth
-echo "#sample_ID, Ref_name, merge_status, total_reads, avg_depth, map_percentage, #_of_mapped_reads, mapped_reads/trimmed_reads" > depth_percentage.txt
+echo "#sample_ID, Ref_name, merge_status, total_reads, avg_depth, aligned_percentage, #_of_mapped_reads, mapped_percentage" > depth_percentage.txt
 # */ is list of directories in current directory
 for reference in */; do
 	# -o is $wd because I want one text file for all reference diretories.
