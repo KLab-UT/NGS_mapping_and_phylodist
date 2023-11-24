a2e7ba3cb2ab7c70204a6388fc0bde129b8fcf44
Dantebob <dantecelani@gmail.com>
2023-11-24 16:33:13 -0700

    changed header columns names made in add_phylodist_column.sh

--- add_phylodist_column.sh
+++ add_phylodist_column.sh
@@ -5,7 +5,7 @@ source_file_1="sample_dist.txt"
 source_file_2="Merged_mapped_percentage.txt"
 #source_file_2=$(sed '1d' depth_percentage.txt)
 
-echo "#sample_ID, Ref_name, number_of_reads, avg_depth, map_percentage, patristic_distance" > $destination_file
+echo "#Sample_ID, Ref_name, Total_reads, Avg_depth, Map_percentage, Patristic_distance" > $destination_file
 
 while IFS="," read -r col_1a col_2a col_3a; do
     while IFS="," read -r col_1b col_2b col_3b col_4b col_5b col_6b; do
