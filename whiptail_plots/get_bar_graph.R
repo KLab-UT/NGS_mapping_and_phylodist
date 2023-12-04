0df6cb8ff704a8f09ebdf6961d027bf04b61b25e
Dantebob <dantecelani@gmail.com>
2023-12-03 18:48:26 -0700

    get_bar_graph.R: added function to make top_3 bar plots for avg_depth and #_mapped_reads. Added top_3_map_percentage.csv

--- whiptail_plots/get_bar_graph.R
+++ whiptail_plots/get_bar_graph.R
@@ -14,16 +14,48 @@ library(ggplot2)
 #   show.legend = NA,
 #   inherit.aes = TRUE
 # )
+
 file_path <- "Map_percentage_and_phylodist_sorted.csv"
 data <- read.csv(file_path) 
-mapped_percentages <- data[, 5]
+map_percentages <- data[, 5]
 species_names <- data[, 1]
-#geom_bar(depth_percentage)
+#geom_bar(map_percentage)
 par(mar = c(14, 4, 4, 0))
-barplot(mapped_percentages,
+par(cex.axis = 1.0)
+barplot(map_percentages,
         names.arg = species_names,
         col = "green",
         main = "Comparison of Species to Whiptails",
         xlab = "",
-        ylab = "Mapped Percentage",
+        ylab = "Map Percentage",
         las = 2)  # Rotates x-axis labels vertically for readability
+
+file_path <- "top_3_map_percentage.csv"
+data <- read.csv(file_path) 
+map_reads <- data[, 4]
+species_names <- data[, 1]
+#geom_bar(map_reads)
+par(mar = c(4, 4, 2, 0))
+par(cex.axis = 0.7)
+barplot(map_reads,
+        names.arg = species_names,
+        col = "green",
+        main = "Comparison of #_of_Mapped_reads",
+        xlab = "",
+        ylab = "# of Mapped Reads")
+        #las = 2)  # Rotates x-axis labels vertically for readability
+
+file_path <- "top_3_map_percentage.csv"
+data <- read.csv(file_path) 
+avg_depth <- data[, 6]
+species_names <- data[, 1]
+#geom_bar(avg_depth)
+par(mar = c(4, 4, 2, 0))
+par(cex.axis = 0.7)
+barplot(avg_depth,
+        names.arg = species_names,
+        col = "green",
+        main = "Comparison of Avg_depth",
+        xlab = "",
+        ylab = "Average Depth")
+        #las = 2)  # Rotates x-axis labels vertically for readability
