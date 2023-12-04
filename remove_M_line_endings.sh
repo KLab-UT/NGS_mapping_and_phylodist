5272aba7f9e5ffd3159ac590d3c8e87e54537d55
Dantebob <dantecelani@gmail.com>
2023-12-03 20:37:51 -0700

    remove_M_line_endings.sh: edited intemediate temp file.

--- remove_M_line_endings.sh
+++ remove_M_line_endings.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-sed 's/\r$//g' depth_and_dist.txt > .remove_M_line_endings_intermediate.txt 
+sed 's/\r$//g' depth_and_dist.txt > .temp_remove_M_line_endings.txt 
 cp .remove_M_line_endings_intermediate.txt depth_and_dist.txt
 
 # \r: This represents the carriage return character (commonly used for line endings in Windows text files).
