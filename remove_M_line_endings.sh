cee355337dbc170e83264887d5f44e51f0ac3323
Dantebob <dantecelani@gmail.com>
2023-12-03 20:39:57 -0700

    remove_M_line_endings.sh: removes .temp file when done.

--- remove_M_line_endings.sh
+++ remove_M_line_endings.sh
@@ -3,5 +3,6 @@
 sed 's/\r$//g' depth_and_dist.txt > .temp_remove_M_line_endings.txt 
 cp .remove_M_line_endings_intermediate.txt depth_and_dist.txt
 
+rm .temp_remove_M_line_endings.txt
 # \r: This represents the carriage return character (commonly used for line endings in Windows text files).
 # $: This matches the end of a line.
