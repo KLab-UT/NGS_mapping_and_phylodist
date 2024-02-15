de72e2174049dd2e5c6b967d573f394455def360
KLab-UT <utt.klab@gmail.com>
2024-02-15 02:11:47 -0700

    added new components to whiptail plots

--- whiptail_plots/make_plots.r
+++ whiptail_plots/make_plots.r
@@ -11,7 +11,8 @@ head(dat)
 #dat$log_dist = log(dat$dist)
 
 # Phylogenetic distance ~ Mapping success
-mapping_dist <- ggplot(dat, aes(Patristic_distance, Mapped_percentage)) + geom_point(size = 3) + geom_smooth(span = 0.5)
+mapping_dist <- ggplot(dat, aes(Patristic_distance, Mapped_percentage)) + geom_point(size = 3) + geom_smooth() + theme_classic()
+log_mapping_dist <- ggplot(dat, aes(log_patristic_distance, log(Mapped_percentage))) + geom_point(size = 3) + geom_smooth() + theme_classic()
 depth_dist <- ggplot(dat, aes(patristic_distance, avg_depth)) + geom_point(size = 3) + geom_smooth(span = 0.5)
 reads_dist <- ggplot(dat, aes(patristic_distance, number_of_reads)) + geom_point(size = 3) 
 
